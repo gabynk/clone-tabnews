14eca094a90a73da87f420dde2e87acf23b8e2c1
Gabriele Nakassima <gaby.naka@gmail.com>
2024-12-09 21:46:45 +0000

    add initial page

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <h1>
+            Algumas mentes são como quebra-cabeças com peças extraordinárias; mesmo que demore a encaixar, o resultado é uma obra-prima única.
+        </h1>
+    )
+}
+
+export default Home
